0a659f497190cb807657e387895280712b7432b5
minhwan <akic4op4@gmail.com>
2024-07-10 23:55:11 +0900

    Add post form submit and login guard

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   def new
     @post = Post.new
+    return if session[:draft_post].nil?
+
+    @post.body = session[:draft_post]
+    session.delete(:draft_post)
   end
 
   def show
@@ -8,6 +12,13 @@ class PostsController < ApplicationController
   end
 
   def create
+    if current_user.nil?
+      session[:draft_post] = post_params[:body]
+      redirect_to new_user_session_path,
+                  notice: 'Please sign in to continue.'
+      return
+    end
+
     @post = Post.new(post_params)
     @post.user_id = current_user.id
 
@@ -20,7 +31,10 @@ class PostsController < ApplicationController
 
   private
 
-    def post_params
-      params.require(:post).permit(:body)
-    end
+  def post_params
+    params.require(:post).permit(:body)
+  end
+
+  def guard_user
+  end
 end
